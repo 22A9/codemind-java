e6133ae0ddca9f013d2347a49e7c849d62ecc311
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-24 10:13:27 +0530

    import java.util.*; public class Division {     public static void main(String args[]) {         Scanner obj = new Scanner(System.in);         int x = obj.nextInt();         int y = obj.nextInt();         int Division = (x / y);         System.out.print(Division); } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Division {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        int x = obj.nextInt();
+        int y = obj.nextInt();
+        int Division = (x / y);
+        System.out.print(Division);
+}
+}
