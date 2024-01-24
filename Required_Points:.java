8af32a5c54d2bdc422741144cecceec80a5cee0c
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-24 22:00:59 +0530

    import java.util.*; public class points {     public static void main(String[] args)     {         Scanner obj = new Scanner(System.in);         int a = obj.nextInt();         int b = obj.nextInt();         int sum = (b / a);         System.out.println(sum);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class points
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int sum = (b / a);
+        System.out.println(sum);
+    }
+}
