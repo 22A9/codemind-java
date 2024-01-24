109a57d0b77fc31e1dd1f6557c764dda02558f21
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-24 22:08:53 +0530

    import java.util.*; public class Noodles {     public static void main(String[] args)     {         Scanner obj = new Scanner(System.in);         int X = obj.nextInt();         int Y = obj.nextInt();         int Ravi = (X * Y);         System.out.println(Ravi);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Noodles 
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int X = obj.nextInt();
+        int Y = obj.nextInt();
+        int Ravi = (X * Y);
+        System.out.println(Ravi);
+    }
+}
