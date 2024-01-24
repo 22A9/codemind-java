17e79f243b4c12f6ab393751bd3f8d1953ae2dfd
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-24 10:46:24 +0530

    import java.util.*; public class King {     public static void main(String[] args) {         Scanner obj = new Scanner(System.in);         int x = obj.nextInt();         int y = obj.nextInt();         int sum = ((x * 5) + (y * 7));         System.out.println(sum);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class King {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int x = obj.nextInt();
+        int y = obj.nextInt();
+        int sum = ((x * 5) + (y * 7));
+        System.out.println(sum);
+    }
+}
