2f62432d723ab3cc40ee7e96f59d608dc90b12f4
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-24 10:17:45 +0530

    import java.util.*; public class Multiplication {     public static void main(String args[]) {         Scanner obj = new Scanner(System.in);         int a = obj.nextInt();         int b = obj.nextInt();         int product = ( a * b);         System.out.println(product);     } }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Multiplication {
+    public static void main(String args[]) {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int product = ( a * b);
+        System.out.println(product);
+    }
+}
