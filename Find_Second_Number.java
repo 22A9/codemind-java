d17854ed50410e5173eb66b95d81fbcfd9a1a3f3
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-24 22:13:59 +0530

    import java.util.*; public class Average {     public static void main(String[] args)     {         Scanner obj = new Scanner(System.in);         int avg = obj.nextInt();         int a = obj.nextInt();         int b = (avg * 2) - a;         System.out.println(b);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Average 
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int avg = obj.nextInt();
+        int a = obj.nextInt();
+        int b = (avg * 2) - a;
+        System.out.println(b);
+    }
+}
