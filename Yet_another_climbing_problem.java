70eb885f9eaa9c4c91310dd4076159621cc103ab
22A9 <119840106+22A9@users.noreply.github.com>
2024-01-31 10:58:36 +0530

    import java.util.*; public class Climb {     public static void main(String[] args)     {         Scanner obj = new Scanner(System.in);         int t = obj.nextInt();         for(int i=0;i<t;i++){             int a = obj.nextInt();             int b = obj.nextInt();
                int q = a / b;
                int r = a % b;
            System.out.println(q+r);
                
            }
        }
    }

---
+++ Yet_another_climbing_problem.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Climb
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int t = obj.nextInt();
+        for(int i=0;i<t;i++){
+            int a = obj.nextInt();
+            int b = obj.nextInt();
+            
+            int q = a / b;
+            int r = a % b;
+        System.out.println(q+r);
+            
+        }
+    }
+}
