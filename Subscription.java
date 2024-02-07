7223bd087d34d8b8bc910b44daeac9801ff2b770
22A9 <119840106+22A9@users.noreply.github.com>
2024-02-07 10:01:36 +0530

    import java.util.*; public class Sub {     public static void main(String[] args)     {         Scanner obj = new Scanner(System.in);         int X = obj.nextInt();         if(X>40)         {             System.out.println("YES");         }         else {             System.out.println("NO");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sub
+{
+    public static void main(String[] args)
+    {
+        Scanner obj = new Scanner(System.in);
+        int X = obj.nextInt();
+        if(X>40)
+        {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
