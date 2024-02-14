3db8f16a5231191328d01fbb97038a89d3b9d432
22A9 <119840106+22A9@users.noreply.github.com>
2024-02-14 11:01:50 +0530

    import java.util.*; public class MyClass {     public static void main(String[] args)     {         Scanner obj = new Scanner(System.in);         int t = obj.nextInt();         while(t--> 0) {             int a = obj.nextInt();             int b = obj.nextInt();
                int z = b-a;
                if(z >=a || a == b) System.out.println("YES");
                else System.out.println("NO");
            }
        }
    }

--- Can_be_a_factor?.java
+++ Can_be_a_factor?.java
@@ -1,16 +1,16 @@
 import java.util.*;
-public class MyClass
+public class MyClass 
 {
     public static void main(String[] args)
     {
         Scanner obj = new Scanner(System.in);
         int t = obj.nextInt();
-        while (t--> 0) {
+        while(t--> 0) {
             int a = obj.nextInt();
             int b = obj.nextInt();
             
-            int z = b - a;
-            if(z >= a || a == b) System.out.println("YES");
+            int z = b-a;
+            if(z >=a || a == b) System.out.println("YES");
             else System.out.println("NO");
         }
     }
