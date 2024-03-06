663f03f759d80b9936acd12f0f020f3741dd9bb5
22A9 <119840106+22A9@users.noreply.github.com>
2024-03-06 09:57:28 +0530

    import java.util.*;
    public class Cars {
        public static void main(String args[]) {
            Scanner sc = new Scanner(System.in);
            
            int n1 = sc.nextInt();
            int n2 = sc.nextInt();
            int x = sc.nextInt();
            
            int count = 0;
            if(n1 >= n2) {
                System.out.println(-1);
            }
            else {
                int temp = 0;
                while (temp <= x) {
                    x +=n1;
                    temp += n2;
                    count++;
                }
                System.out.println(count);
            }
        }
    }

---
+++ Cars.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Cars {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int x = sc.nextInt();
+        
+        int count = 0;
+        if(n1 >= n2) {
+            System.out.println(-1);
+        }
+        else {
+            int temp = 0;
+            while (temp <= x) {
+                x +=n1;
+                temp += n2;
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
